368ecdddeec4c05293bef4544266a0b2fa858cbf
Borteq2 <borteq2@yandex.ru>
2023-03-15 17:13:30 +0300

    mask

--- 6/main.py
+++ 6/main.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-# photo = cv2.imread('../images/pyos-samolyot.jpg')
-img = np.zeros((350, 350), dtype='uint8')
+photo = cv2.imread('../images/pyos-samolyot.jpg')
+img = np.zeros(photo.shape[:2], dtype='uint8')
 
 circle = cv2.circle(
     img=img.copy(),
-    center=(0, 0),
-    radius=50,
+    center=(200, 300),
+    radius=120,
     color=255,
     thickness=-1,  # filled
 )
@@ -20,10 +20,10 @@ square = cv2.rectangle(
     thickness=-1,  # filled
 )
 
-# img = cv2.bitwise_and(circle, square)
+img = cv2.bitwise_and(photo, photo, mask=circle)
 # img = cv2.bitwise_or(circle, square)
 # img = cv2.bitwise_xor(circle, square)
-img = cv2.bitwise_not(circle)
+# img = cv2.bitwise_not(circle)
 
 # cv2.imshow('Result', circle)
 # cv2.imshow('Result', square)
